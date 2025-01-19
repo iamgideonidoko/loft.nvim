9640f56b7398f487198807f8eda4184ee48564b8
Gideon Idoko <iamgideonidoko@gmail.com>
2025-01-19 05:58:42 +0100

    feat(actions): add function to close a buffer

---
+++ lua/loft/actions.lua
@@ -0,0 +1,50 @@
+local registry_instance = require("loft.registry")
+local utils = require("loft.utils")
+
+local actions = {}
+
+---Delete a buffer without closing splits
+---@param opts { force: boolean?, buffer: integer? }?
+actions.close_buffer = function(opts)
+  opts = opts or {}
+  local current_buf = opts.buffer or vim.api.nvim_get_current_buf()
+  if not opts.force and vim.api.nvim_get_option_value("modified", { buf = current_buf }) then
+    return vim.api.nvim_err_writeln("Buffer is modified. Force required.")
+  end
+  if not opts.force and vim.api.nvim_get_option_value("buftype", { buf = current_buf }) == "terminal" then
+    return vim.api.nvim_err_writeln("Buffer is a terminal. Force required.")
+  end
+  local alt_buf = vim.fn.bufnr("#")
+  local all_valid_buffers = utils.get_all_valid_buffers()
+  local next_buf = nil
+  local found_current = false
+  for _, buf in ipairs(all_valid_buffers) do
+    if found_current then
+      next_buf = buf
+      break
+    end
+    if buf == current_buf then
+      found_current = true
+    end
+  end
+  if #all_valid_buffers > 1 and not next_buf then
+    -- The next valid buffer is likely the first
+    next_buf = all_valid_buffers[1]
+  end
+  registry_instance:pause_update()
+  -- Replace current buffer with alt or next or empty buffer in all windows
+  for _, win in ipairs(vim.fn.win_findbuf(current_buf)) do
+    if utils.is_buffer_valid(alt_buf) then
+      vim.api.nvim_win_set_buf(win, alt_buf)
+    elseif next_buf then
+      vim.api.nvim_win_set_buf(win, next_buf)
+    else
+      vim.api.nvim_win_set_buf(win, vim.api.nvim_create_buf(false, true))
+    end
+  end
+  pcall(vim.api.nvim_buf_delete, current_buf, { force = opts.force })
+  registry_instance:clean()
+  registry_instance:resume_update()
+end
+
+return actions
