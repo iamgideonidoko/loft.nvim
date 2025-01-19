6002dbdca0d06364e622c0d229cfad6924c45c2e
Gideon Idoko <iamgideonidoko@gmail.com>
2025-01-19 03:01:44 +0100

    feat: add util to check if given or curr win is floating

--- lua/loft/utils.lua
+++ lua/loft/utils.lua
@@ -6,4 +6,12 @@ utils.is_dev = function()
   return require("lazy.core.config").plugins[constants.PLUGIN_NAME].dev
 end
 
+---Check if the given or current or window is a floating
+---@param window number?
+utils.is_floating_window = function(window)
+  local win_id = window or vim.api.nvim_get_current_win()
+  local win_config = vim.api.nvim_win_get_config(win_id)
+  return win_config.relative ~= ""
+end
+
 return utils
