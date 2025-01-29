3473d0abe78c247861b01eb95b89ac3d0808c138
Gideon Idoko <iamgideonidoko@gmail.com>
2025-01-29 05:25:10 +0100

    feat(commands): add another to toggle mark state of current buffer

--- lua/loft/commands.lua
+++ lua/loft/commands.lua
@@ -12,6 +12,12 @@ commands.setup = function()
     actions.toggle_smart_order.func,
     { desc = actions.toggle_smart_order.desc }
   )
+
+  vim.api.nvim_create_user_command(
+    "LoftToggleMark",
+    actions.toggle_mark_current_buffer.func,
+    { desc = actions.toggle_mark_current_buffer.desc }
+  )
 end
 
 return commands
