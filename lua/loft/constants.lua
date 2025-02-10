0cf45768ac5b7e38e80d4a11811d0e8019f592e3
Gideon Idoko <iamgideonidoko@gmail.com>
2025-02-10 04:15:40 +0100

    Release v0.1.0

--- lua/loft/constants.lua
+++ lua/loft/constants.lua
@@ -2,7 +2,7 @@ local constants = {}
 
 constants.DISPLAY_NAME = "Loft"
 constants.PLUGIN_NAME = "loft.nvim"
-constants.PLUGIN_VERSION = "0.0.1"
+constants.PLUGIN_VERSION = "0.1.0"
 constants.MARK_STATE_ID = "loft_buffer_mark_state"
 
 return constants
